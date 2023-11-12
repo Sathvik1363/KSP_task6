f9c1b2bc9545c09bda9d20ccb1b0f7af65fbb877
SathvikReddy1363 <133073341+Sathvik1363@users.noreply.github.com>
2023-11-12 22:24:44 +0530

    Add files via upload

---
+++ t6/t6.js
@@ -0,0 +1,82 @@
+// // Arithmetic operators
+let num1 = 10;
+let num2 = 5;
+// // Addition
+console.log(num1 + num2);
+// // Subtraction
+console.log(num1 - num2);
+// // Multiplication
+console.log(num1 * num2);
+// // Division
+console.log(num1 / num2);
+// // Modulus (remainder)
+console.log(num1 % num2);
+
+console.log("2"+"2");
+
+// // Assignmennt operators:
+let num3  = 100;
+num3 += 20;
+console.log(num3);
+num3 -= 40;
+console.log(num3);
+num3 *= 8;
+console.log(num3);
+num3 /= 2;
+console.log(num3);
+num3 %= 7;
+console.log(num3);
+num3 **= 3;
+console.log(num3);
+
+// // Comparision operators
+let n1 = 50;
+let name = "50"
+let n2 = 55;
+console.log(n1==name);
+console.log(n1===name);
+console.log(n1!=name);
+console.log(n1<=n2);
+console.log(n1<n2);
+console.log(n1>=n2);
+
+// // logical operators
+let x = true;
+let y = false;
+console.log(!x);
+console.log(x && y);
+console.log(x || y);
+
+// functions
+function sathvik(){
+    console.log("I am Sathvik");
+}
+sathvik()
+
+let nm1 = parseInt(prompt("Enter num1:"));
+let nm2 = parseInt(prompt("Enter num2:"));
+function Addition(){
+    console.log(nm1+nm2);
+}   
+
+function Subtraction(){
+    console.log(nm1-nm2);
+}   
+
+function Multiplication(){
+    console.log(nm1*nm2);
+}  
+function Division(){
+    console.log(nm1/nm2);
+}
+Addition(nm1,nm2);
+Subtraction(nm1,nm2);
+Multiplication(nm1,nm2);
+Division(nm1,nm2);
+
+
+
+
+
+
+
